468ab75d88e0f73f2977f94bbec4f712398233ae
otho <contact@otho.bike>
2021-07-08 15:59:59 +0200

    (fix) Fix registration form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -18,7 +18,8 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('name')
+            ->add('firstName')
+            ->add('lastName')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
